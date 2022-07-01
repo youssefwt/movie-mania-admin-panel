1a16c6d467fd5540f1504d14e451002930e50727
youssefwt <Ywtalaat@gmail.com>
2022-07-01 16:06:10 +0200

    adding new movie

--- src/pages/newProduct/NewProduct.jsx
+++ src/pages/newProduct/NewProduct.jsx
@@ -1,28 +1,108 @@
+import { useState } from "react";
 import "./newProduct.css";
 
 export default function NewProduct() {
+  const [movie, setMovie] = useState({});
+  const [image, setImage] = useState(null);
+  const [trailer, setTrialer] = useState(null);
+  const [video, setVideo] = useState(null);
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setMovie({ ...movie, [e.target.name]: value });
+  };
+
+  console.log(image);
+
   return (
     <div className="newProduct">
-      <h1 className="addProductTitle">New Product</h1>
+      <h1 className="addProductTitle">New Movie</h1>
       <form className="addProductForm">
         <div className="addProductItem">
           <label>Image</label>
+          <input
+            type="file"
+            id="image"
+            name="image"
+            onChange={(e) => setImage(e.target.files[0])}
+          />
+        </div>
+        {/* <div className="addProductItem">
+          <label>Tilte image</label>
           <input type="file" id="file" />
         </div>
         <div className="addProductItem">
-          <label>Name</label>
-          <input type="text" placeholder="Apple Airpods" />
+          <label>thumbnail image</label>
+          <input type="file" id="file" />
+        </div> */}
+        <div className="addProductItem">
+          <label>Title</label>
+          <input
+            type="text"
+            placeholder="movie name"
+            name="title"
+            onChange={handleChange}
+          />
         </div>
         <div className="addProductItem">
-          <label>Stock</label>
-          <input type="text" placeholder="123" />
+          <label>Description</label>
+          <input
+            type="text"
+            placeholder="desscription"
+            name="desscription"
+            onChange={handleChange}
+          />
         </div>
         <div className="addProductItem">
-          <label>Active</label>
-          <select name="active" id="active">
-            <option value="yes">Yes</option>
-            <option value="no">No</option>
+          <label>Year</label>
+          <input
+            type="text"
+            placeholder="year"
+            name="year"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="addProductItem">
+          <label>Genre</label>
+          <input
+            type="text"
+            placeholder="genre"
+            name="genre"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="addProductItem">
+          <label>Duration</label>
+          <input
+            type="text"
+            placeholder="duration"
+            name="duration"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="addProductItem">
+          <label>Limit</label>
+          <input
+            type="text"
+            placeholder="limit"
+            name="limit"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="addProductItem">
+          <label>is series?</label>
+          <select name="isSeries" id="isSeries" onChange={handleChange}>
+            <option value="false">no</option>
+            <option value="true">yes</option>
           </select>
+        </div>
+        <div className="addProductItem">
+          <label>Trailer</label>
+          <input type="file" name="trailer" />
+        </div>
+        <div className="addProductItem">
+          <label>Video</label>
+          <input type="file" name="trailer" />
         </div>
         <button className="addProductButton">Create</button>
       </form>
